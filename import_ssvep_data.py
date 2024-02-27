8011db40d41f749adb5e3269c14999a5fd9cf31b
Claire Leahy <cleahy@uvm.edu>
2024-02-26 20:26:05 -0500

    Part 1 (loading function)

--- import_ssvep_data.py
+++ import_ssvep_data.py
@@ -3,6 +3,40 @@
 """
 Created on Sun Feb 25 22:00:05 2024
 
-@author: ClaireLeahy
+@author: Peijin Chen and Claire Leahy
+
+Sources:
+    
+    Avoiding printing "dict_keys" data type: https://blog.finxter.com/python-print-dictionary-keys-without-dict_keys/
+    
 """
 
+# import statements
+import numpy as np
+from matplotlib import pyplot as plt
+import scipy.fft
+
+#%% Part 1: Load the Data
+def load_ssvep_data(subject, data_directory='SsvepData/'):
+    
+    # load data
+    data = np.load(f'{data_directory}/SSVEP_S{subject}.npz', allow_pickle=True)
+    
+    # create data dictionary
+    data_dict = {'eeg': data['eeg'], 'channels': data['channels'], 'fs': data['fs'], 'event_samples': data['event_samples'], 'event_durations': data['event_durations'], 'event_types': data['event_types']}
+    
+    # printing to inform user of some data features
+    print(f'Data keys: {list(data_dict.keys())}') # list conversion prevents printing data type
+    print('\nChannels: ', data['channels'])
+    print('\nSampling frequency (Hz):', data['fs'])
+    
+    # return data dictionary
+    return data_dict
+
+#%% Part 2: Plot the Data
+
+#%% Part 3: Extract the Epochs
+
+#%% Part 4: Take the Fourier Transform
+
+#%% Part 5: Plot the Power Spectra
--- test_import_ssvep_data.py
+++ test_import_ssvep_data.py
@@ -3,6 +3,15 @@
 """
 Created on Sun Feb 25 22:02:31 2024
 
-@author: ClaireLeahy
+@author: Peijin Chen and Claire Leahy
 """
 
+# import statements
+from import_ssvep_data import load_ssvep_data
+
+#%% Part 1: Load the Data
+
+# load data for subject 1
+data_dict = load_ssvep_data(1, data_directory='SsvepData/')
+
+#%% Part 6: Reflect
