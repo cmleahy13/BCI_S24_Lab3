93e6c74f66a14bcd0feec9f3052375969b8bc4d7
cmleahy13 <83472824+cmleahy13@users.noreply.github.com>
2024-02-27 22:44:50 -0500

    Add files via upload

--- import_ssvep_data.py
+++ import_ssvep_data.py
@@ -9,6 +9,8 @@ Sources:
     
     Avoiding printing "dict_keys" data type: https://blog.finxter.com/python-print-dictionary-keys-without-dict_keys/
     
+    Setting axis tickmarks: ChatGPT
+    
 """
 
 # import statements
@@ -20,9 +22,9 @@ import scipy.fft
 def load_ssvep_data(subject, data_directory='SsvepData/'):
     
     # load data
-    data = np.load(f'{data_directory}/SSVEP_S{subject}.npz', allow_pickle=True)
+    data = np.load(f'{data_directory}SSVEP_S{subject}.npz', allow_pickle=True)
     
-    # create data dictionary
+    # create data dictionary (convert existing data type to dict)
     data_dict = {'eeg': data['eeg'], 'channels': data['channels'], 'fs': data['fs'], 'event_samples': data['event_samples'], 'event_durations': data['event_durations'], 'event_types': data['event_types']}
     
     # printing to inform user of some data features
@@ -34,68 +36,196 @@ def load_ssvep_data(subject, data_directory='SsvepData/'):
     return data_dict
 
 #%% Part 2: Plot the Data
+
 def plot_raw_data(data, subject, channels_to_plot):
     
-    #extract data
+    # extract data
+    eeg = data['eeg']
+    channels = list(data['channels'])
+    fs = data['fs']
     event_durations = data['event_durations']
     event_samples = data['event_samples']
     event_types = data['event_types']
-    all_channels = list(data['channels'])
     
-    #compute event intervals and start/finish times
-    event_intervals = []
+    # find sample times
+    t = [] # declare list to contain each sample time in seconds
+    # load in times in seconds for all samples
+    for sample_index in range(len(eeg.T)):
+        
+        t.append(sample_index*(1/fs)) # add to time list by dividing sample number by sample frequency
+    
+    # find event samples and times
+    event_intervals = [] # list to contain samples for event intervals
+    # find event intervals using corresponding sample index and duration
     for sample_index in range(len(event_samples)):
       
         event_start = event_samples[sample_index]
-        event_finish = int(event_samples[sample_index] + event_durations[sample_index])
+        event_finish = int(event_samples[sample_index]+event_durations[sample_index])
         
         event_intervals.append((event_start, event_finish))
-    
-    # define time axis
-    t = np.arange(len(data['eeg'].T)) # tentative, number of samples
+        
+    # times in seconds for event samples
+    for sample_index in range(len(event_samples)):
+        
+        event_intervals[sample_index] = np.array(event_intervals[sample_index])/fs # convert each event sample number to a time, must convert to array first
     
     # initialize figure
-    fig, ax = plt.subplots(2, sharex=True)
+    figure, sub_figure = plt.subplots(2, sharex=True)
     
-    # top subplot to contain event start/end times, types
+    # top subplot containing flash frequency over span of event
+    for event_number, interval in enumerate(event_intervals):
     
-    # set plot title
-    ax[0].set_title(f'SSVEP Subject {subject} Raw Data')
-    
-    #subplot 1
-    for event_num, interval in enumerate(event_intervals):
-    
-        if event_types[event_num] == "12hz":
-            ax[0].hlines(xmin = interval[0], xmax = interval[1], y = 12,label = 'o')
-            ax[0].plot([interval[0], interval[1]], [12,12], 'bo')
+        if event_types[event_number] == "12hz":
+            sub_figure[0].hlines(xmin = interval[0], xmax = interval[1], y = 12,label = 'o')
+            sub_figure[0].plot([interval[0], interval[1]], [12,12], 'bo')
     
         else:
-            ax[0].hlines(xmin = interval[0], xmax = interval[1], y = 15,label = 'o')
-            ax[0].plot([interval[0], interval[1]], [15,15], 'bo')
+            sub_figure[0].hlines(xmin = interval[0], xmax = interval[1], y = 15,label = 'o')
+            sub_figure[0].plot([interval[0], interval[1]], [15,15], 'bo')
     
-    ax[0].set_xlabel('time(s)')
-    ax[0].grid(alpha=0.2,color="black")
-    #ax[0].vlines([x if x % 1000 == 0 else None for x in range(data['eeg'].shape[-1])], ymin = 12, ymax = 15)
-      
-    # bottom subplot to contain raw data from specified electrodes
+    # bottom subplot contain raw data from specified electrodes
     for channel_number, channel_name in enumerate(channels_to_plot):
       
-        channel_index = all_channels.index(channel_name) # index of channel of interest considering all channels
+        channel_index = channels.index(channel_name) # index of channel of interest considering all channels
         
-        eeg_data = data['eeg'][channel_index] # define EEG data for the channel
+        eeg_data = data['eeg'][channel_index] # EEG data for the channel
     
-        ax[1].plot(t, eeg_data*(10**6)) # plot EEG data in uV from given channel
-        #ax[1].set_title(f"Channel {channel} EEG plot")
+        sub_figure[1].plot(t, eeg_data*(10**6), label=channel_name) # plot EEG data in µV from channel
     
     # format figure
-    fig.tight_layout()
-    ax[1].set_xlabel('time (s)')
+    # subplot 1
+    sub_figure[0].set_xlabel('time(s)')
+    sub_figure[0].set_ylabel('Flash frequency')
+    sub_figure[0].set_yticks([12,15])
+    sub_figure[0].set_yticklabels(['12Hz','15Hz'])
+    sub_figure[0].grid()
+    
+    # subplot 2
+    sub_figure[1].set_xlabel('time (s)')
+    sub_figure[1].set_ylabel('Voltage (µV)')
+    sub_figure[1].legend(loc='best') # place legend in best location given data
+    sub_figure[1].grid()
+    
+    # set plot title
+    figure.suptitle(f'SSVEP Subject {subject} Raw Data')
+    
+    # general appearance
+    figure.tight_layout()
     
     # save image
     plt.savefig(f'SSVEP_S{subject}_rawdata.png')
 
 #%% Part 3: Extract the Epochs
 
+def epoch_ssvep_data(data_dict, epoch_start_time=0, epoch_end_time=20):
+  
+    # extract data
+    eeg = data_dict['eeg']
+    channels = list(data_dict['channels']) # convert to list
+    fs = data_dict['fs']
+    event_durations = data_dict['event_durations']
+    event_samples = data_dict['event_samples']
+    event_types = data_dict['event_types']
+    time_per_epoch = int(fs*(epoch_end_time-epoch_start_time)) # convert to int
+    
+    list_of_channel_epoch_arrays = []
+    
+    for channel in channels:
+        
+        channel_index = channels.index(channel)
+        eeg_data = eeg[channel_index]
+        epoch_list = [] 
+        
+        for sample_index in range(len(event_samples)):
+            start = event_samples[sample_index]
+            end = event_samples[sample_index] + int(event_durations[sample_index])
+            epoch_index = eeg_data[start:end]
+            epoch_list.append(epoch_index)
+    
+        epoch_array = np.stack(epoch_list)
+    
+        list_of_channel_epoch_arrays.append(epoch_array)
+   
+    eeg_epochs = np.stack(list_of_channel_epoch_arrays, axis=1)
+    
+    epoch_times = np.linspace(epoch_start_time, epoch_end_time, time_per_epoch)
+    
+    is_trial_15Hz = [True if event == '15hz' else False for event in event_types]
+    
+    is_trial_15Hz = np.array(is_trial_15Hz)
+    
+    return eeg_epochs, epoch_times, is_trial_15Hz
+
 #%% Part 4: Take the Fourier Transform
 
+def get_frequency_spectrum(eeg_epochs, fs):
+  
+    # take the Fourier Transform of the epoched EEG data
+    eeg_epochs_fft = np.fft.rfft(eeg_epochs)
+    
+    # find the corresponding frequencies from the epoched EEG data
+    fft_frequencies = np.fft.rfftfreq(n=eeg_epochs.shape[-1], d=1/fs) # n is the number of samples in the signal (dimension 2 (final dimension) in eeg_epochs), d is the inverse of sampling frequency
+  
+    return eeg_epochs_fft, fft_frequencies
+
 #%% Part 5: Plot the Power Spectra
+
+def plot_power_spectrum(eeg_epochs_fft, fft_frequencies, is_trial_15Hz, channels, channels_to_plot, subject):
+
+    # convert channels to list
+    channels = list(channels)
+    
+    # calculating power spectra
+    # isolate frequency spectra by event type (12Hz or 15Hz)
+    event_15 = eeg_epochs_fft[is_trial_15Hz]
+    event_12 = eeg_epochs_fft[~is_trial_15Hz]
+    
+    # calculate power for event type
+    event_15_power = (np.abs(event_15))**2
+    event_12_power = (np.abs(event_12))**2 
+    
+    # calculate mean power for event type
+    event_15_power_mean = event_15_power.mean(0)
+    event_12_power_mean = event_12_power.mean(0)
+    
+    # calculate normalized power for event type
+    normalized_event_15_power_mean = event_15_power_mean/(np.max(event_15_power_mean))
+    normalized_event_12_power_mean = event_12_power_mean/(np.max(event_12_power_mean))
+    
+    # calculate spectra for event type
+    spectrum_db_15Hz = 10*(np.log10(normalized_event_15_power_mean))
+    spectrum_db_12Hz = 10*(np.log10(normalized_event_12_power_mean))
+    
+    # plotting
+    # isolate channel being plotted
+    channel_to_plot = [channels.index(channel_name) for channel_name in channels_to_plot]
+    
+    # set up figure
+    figure, channel_plot = plt.subplots(len(channels_to_plot))
+    
+    for plot_index, channel in enumerate(channel_to_plot): # plot_index as a means of accessing a subplot
+        
+        # plot the power spectra by event type
+        channel_plot[plot_index].plot(fft_frequencies, spectrum_db_12Hz[channel,:], color='red')
+        channel_plot[plot_index].plot(fft_frequencies, spectrum_db_15Hz[channel,:], color='green')
+        
+        # formatting subplot
+        channel_plot[plot_index].set_xlim(0,80)
+        channel_plot[plot_index].set_xlabel('frequency (Hz)')
+        channel_plot[plot_index].set_ylabel('power (dB)')
+        channel_plot[plot_index].set_title(f'Channel {channels_to_plot[plot_index]}')
+        channel_plot[plot_index].legend(['12Hz','15Hz'], loc='best')
+        channel_plot[plot_index].grid()
+        
+        # plot dotted lines at 12Hz and 15Hz
+        channel_plot[plot_index].axvline(12, color='red', linestyle='dotted')
+        channel_plot[plot_index].axvline(15, color='green', linestyle='dotted')
+    
+    # format overall plot
+    figure.suptitle(f'SSVEP Subject S{subject} Frequency Content')
+    figure.tight_layout()
+    
+    # save image
+    plt.savefig(f'SSVEP_S{subject}_frequency_content.png')
+    
+    return spectrum_db_15Hz, spectrum_db_12Hz
--- test_import_ssvep_data.py
+++ test_import_ssvep_data.py
@@ -6,8 +6,8 @@ Created on Sun Feb 25 22:02:31 2024
 @author: Peijin Chen and Claire Leahy
 """
 
-# import statements
-from import_ssvep_data import load_ssvep_data, plot_raw_data
+# import applicable functions from module
+from import_ssvep_data import load_ssvep_data, plot_raw_data, epoch_ssvep_data, get_frequency_spectrum, plot_power_spectrum
 
 #%% Part 1: Load the Data
 
@@ -19,4 +19,54 @@ data_dict = load_ssvep_data(subject=1, data_directory='SsvepData/')
 # plot the EEG data from electodes Fz and Oz for subject 1
 plot_raw_data(data=data_dict, subject=1, channels_to_plot=['Fz','Oz'])
 
+
+#%% Part 3: Extract the Epochs
+
+# extract epochs (as written for subject 1)
+eeg_epochs, epoch_times, is_trial_15Hz = epoch_ssvep_data(data_dict, epoch_start_time=0, epoch_end_time=20)
+
+#%% Part 4: Take the Fourier Transform
+
+# get the frequency spectrum (as written for subject 1)
+eeg_epochs_fft, fft_frequencies = get_frequency_spectrum(eeg_epochs, fs=data_dict['fs'])
+
+#%% Part 5: Plot the Power Spectra
+
+# plot the power spectra for electrodes Fz and Oz for subject 1
+spectrum_db_15Hz, spectrum_db_12Hz = plot_power_spectrum(eeg_epochs_fft, fft_frequencies, is_trial_15Hz, channels=data_dict['channels'], channels_to_plot=['Fz','Oz'], subject=1)
+
+#%% Run everything for S2
+
+data_dict2 = load_ssvep_data(subject=2, data_directory='SsvepData/')
+
+plot_raw_data(data=data_dict2, subject=2, channels_to_plot=['Fz','Oz'])
+
+eeg_epochs2, epoch_times2, is_trial_15Hz2 = epoch_ssvep_data(data_dict2, epoch_start_time=0, epoch_end_time=20)
+
+eeg_epochs_fft2, fft_frequencies2 = get_frequency_spectrum(eeg_epochs2, fs=data_dict2['fs'])
+
+spectrum_db_15Hz2, spectrum_db_12Hz2 = plot_power_spectrum(eeg_epochs_fft2, fft_frequencies2, is_trial_15Hz2, channels=data_dict2['channels'], channels_to_plot=['Fz','Oz'], subject=2)
+
+
 #%% Part 6: Reflect
+
+# need to include citations
+
+"""
+1. On some electrodes, there are peaks in the spectra at 12Hz for 12Hz trials and 15Hz for 15Hz trials. What is the name for the brain signal that leads to these peaks? Where in the brain do they originate and why (i.e., what function does this brain region serve)?
+    
+    a. Steady-state visual evoked potentials (SSVEPs) are the signals that provide the peaks in those spectra. Steady-state refers to the repetitive nature of the flashes presented to the BCI user, and visual evoked potentials are voltages (brain signals measured using electroencephalography) that appear in the signal due to a visual external stimulus. Generally, these peaks are observed within the occipital lobe, which is largely responsible for processign visual data.
+
+2. There are smaller peaks at integer multiples of these peak frequencies. What do you call these integer multiples? Why might we see them in the data?
+    
+    a. These peaks at multiples of the event frequencies are known as harmonics. They occur due to neuronal responses to stimuli, as the neurons respond in a nonlinear fashion.
+
+3. There’s a peak in the spectra at 50Hz. What is the most likely cause for this peak?
+    
+    a. This peak at 50Hz is very likely to be an artifact related to utility frequency. In many regions, 50Hz is the frequency of the alternating current electrical supply, and this electricity can interfere with the signal detected by the electrodes.
+
+4. Besides the sharp peaks just described, there’s a frequency spectrum where the power is roughly proportional to 1 over the frequency. This is a common pattern in biological signals and is sometimes called “1/f” or “neural noise” in EEG. But on top of that, there’s a slight upward bump around 10 Hz on some electrodes. What brain signal might cause this extra power at about 10Hz? Which channels is it most prominently observed on, and why might this be?
+    
+    a. Alpha waves?
+
+"""
